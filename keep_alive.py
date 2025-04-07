dbd0e121d189e57d8bd1f7c7a6604723bfe883fc
Mikotakegou2001 <tranthimailin2001@gmail.com>
2025-04-07 17:49:12 +0700

    Create keep_alive.py

---
+++ keep_alive.py
@@ -0,0 +1,18 @@
+from keep_alive import keep_alive
+keep_alive()
+
+import os
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}')
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Pong!")
+
+bot.run(os.getenv("TOKEN"))
